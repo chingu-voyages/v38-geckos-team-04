142c229fcb23cccd32df92910e97e142217eb375
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-03-27 19:31:27 +0100

    Update Footer component

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+function Footer() {
+  return <Fragment>
+            <footer className='footer'>
+                <p>footer content</p>
+            </footer>
+        </Fragment>
+  
+}
+
+export default Footer
