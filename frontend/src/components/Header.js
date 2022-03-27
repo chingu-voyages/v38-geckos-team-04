e22f05a4176b2285eaa1e70f9f49b43f509381d5
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-03-27 19:53:10 +0100

    Update the Header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <header className='header'>
             <div className="header__bg-video">
                 <video className="header__video" autoPlay muted loop>
-
+                    <source  src="../assets/Coding on a laptop.mp4" type="video/mp4"/>
                 </video>
             </div>
             <h1 className='header__heading'>abb - always be building</h1>
