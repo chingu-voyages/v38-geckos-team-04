e72e9421419895f28700e2e187e68bc2c2fe73aa
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-03-27 19:34:18 +0100

    Update Main component

---
+++ frontend/src/components/Main.js
@@ -0,0 +1,11 @@
+import { Fragment }from 'react'
+
+function Main() {
+  return <Fragment>
+            <main className='main'>
+                <p>Hello App Ideas World</p>
+            </main>
+        </Fragment>
+}
+
+export default Main
