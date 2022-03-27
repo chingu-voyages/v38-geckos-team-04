150ca202357882b4d036f37a2cda147ca8372ad1
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-03-27 19:23:39 +0100

    Update Header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,11 @@
 import { Fragment } from "react";
 
 const Header = () => {
-    <>
-    
-    </>
+    <Fragment>
+        <header className='header'>
+            <h1 className='header__heading'>abb - always be building</h1>
+        </header>
+    </Fragment>
 }
 
 export default Header;
