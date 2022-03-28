ad6f4a7b3de95c9a175a5e4e131b5c91bc818bca
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-03-28 05:02:58 +0100

    Create basic html structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 
 const App = () => (
-  <div>
-    <h1>abb - always be building</h1>
-    <p>Hello App Ideas World</p>
-  </div>
+  <>
+    <header>
+      <h1>abb - always be building</h1>
+      <p>Hello App Ideas World</p>
+      <nav>
+      </nav>
+    </header>
+
+    <main>
+    </main>
+
+    <footer>
+    </footer>
+  </>
 )
 
 export default App
