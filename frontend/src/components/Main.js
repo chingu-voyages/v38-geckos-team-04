836b1fabd3d84f94c874cc4bac4a09cc45dacfa2
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-03-28 14:33:30 +0100

    Import searchForm and ResultLists components into Main

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,9 +1,13 @@
 import { Fragment }from 'react'
+import SearchForm from './SearchForm';
+import ResultsList from './ResultsList';
 import '../sass/components/main.scss';
 
 function Main() {
   return <Fragment>
             <main className='main'>
+                <SearchForm />
+                <ResultsList />
                 <p>Hello App Ideas World</p>
             </main>
         </Fragment>
