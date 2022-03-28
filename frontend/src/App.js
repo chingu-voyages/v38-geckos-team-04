4f8a09bb69d7251a7f24fadfdecd25bc8308db80
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-03-28 14:18:40 +0100

    Refactor: Replaced div from Appjs with the shorthand fragment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import SearchForm from './SearchForm';
 import ResultsList from './ResultsList';
 
 const App = () => (
-  <div>
+  <>
     <header>
       <h1>abb - always be building</h1>
       <p>Hello App Ideas World</p>
@@ -17,7 +17,7 @@ const App = () => (
 
     <footer>
     </footer>
-  </div>
+  </>
 )
 
 export default App;
