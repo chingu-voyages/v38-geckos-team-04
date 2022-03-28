f4859043a0d2549981b82fa3de471f61b58c70a0
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-03-28 14:31:27 +0100

    Remove unused components from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import SearchForm from './SearchForm';
-import ResultsList from './ResultsList';
 import './sass/App.scss'
 
 const App = () => (
