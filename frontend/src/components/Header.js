8497adf4229f7fc97ecd97da3fba019aedca99dc
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-03-29 16:12:37 +0100

    Import heading styles into header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import '../sass/components/header.scss';
+import '../sass/base/typography.scss';
 import video from '../assets/coding-video.mp4';
 
 const Header = () => {
