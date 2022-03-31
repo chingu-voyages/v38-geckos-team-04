ea52ad32b8c23862fa09deff28dac5e5b18d13b4
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-03-31 18:36:15 +0100

    Remove dotenv from app.js

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,9 @@
-require('dotenv').config();
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT;
 
 // parse application/json content-type
-app.use(express.json())
+app.use(express.json());
 
 // create server
-app.listen(PORT, () => {
-    console.log(`Now running server on port ${PORT}`);
-});
+app.listen(5000);
