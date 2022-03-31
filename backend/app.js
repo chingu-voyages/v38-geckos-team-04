555f972685be2f3f86e8baacb83c451da46220f1
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-03-31 15:22:09 +0100

    Create development server

--- backend/app.js
+++ backend/app.js
@@ -6,3 +6,8 @@ const PORT = process.env.PORT;
 
 // parse application/json content-type
 app.use(express.json())
+
+// create server
+app.listen(() => {
+    console.log(`Now running on port ${PORT}`);
+}, PORT);
