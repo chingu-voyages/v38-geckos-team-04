a70d4fb8030382b3b3b7862a465475e70426616d
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-03-31 15:19:58 +0100

    Import development port in PORT const

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT;
 
 // parse application/json content-type
 app.use(express.json())
