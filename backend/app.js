f29201876023740ed095ee55c00ee694d2421206
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-03-31 18:48:18 +0100

    Add comments for readability

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,17 @@ app.use(express.json());
 
 // demo response from the server
 app.get("/", (req, res, next) => {
+    // create javascript object
     const dummyContent = {
         name: "App Finder",
         stack: "MERN",
         team: "Jacek, Lauren, Moody",
     };
+
+    // convert object to json
     const dummyJSON = JSON.stringify(dummyContent);
+ 
+    // set the response status and send the json data
     res.status(200).send(dummyJSON);
 })
 
