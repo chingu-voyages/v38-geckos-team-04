a961e8b3aa7560beec46f4739fac998630592ef7
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-03-31 18:47:19 +0100

    Update get route with dummy content

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,17 @@ const app = express();
 // parse application/json content-type
 app.use(express.json());
 
+// demo response from the server
+app.get("/", (req, res, next) => {
+    const dummyContent = {
+        name: "App Finder",
+        stack: "MERN",
+        team: "Jacek, Lauren, Moody",
+    };
+    const dummyJSON = JSON.stringify(dummyContent);
+    res.status(200).send(dummyJSON);
+})
+
 // create server
 app.listen(5000, () => {
     console.log('Visit localhost:5000/ on a browser');
