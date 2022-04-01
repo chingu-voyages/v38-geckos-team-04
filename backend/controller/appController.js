fc90bc2fd6d3af5ffd99d106a9344b0263a596f1
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-01 20:18:30 +0100

    Add documentation of the appController

--- backend/controller/appController.js
+++ backend/controller/appController.js
@@ -2,9 +2,13 @@
 
 /**
  * 
- * @param {Object} req This is the request object 
- * @param {Object} res This is the response object
- * @param {Function} next This function is used to call the next middleware
+ * @param { Object } req This is the request object 
+ * @param { Object } res This is the response object
+ * @param { Function } next This function is used to call the next middleware
+ * 
+ * @description
+ * This function will receive the request and response objects and handle the request
+ * and response for the target route.
  */
 module.exports.appController = (req, res, next) => {
 
