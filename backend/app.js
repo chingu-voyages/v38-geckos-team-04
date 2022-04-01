c99e13999922ee156df653c5a82f4836b198cef7
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-01 20:21:57 +0100

    Import mainRoute

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,17 @@
 const express = require('express');
 
+// import main route
+const main = require('./routes/mainRoute');
+
 // expose the express api into the app const
 const app = express();
+
 
 // parse application/json content-type
 app.use(express.json());
 
-// demo response from the server
-app.get("/", (req, res, next) => {
-    // create dummy javascript object
-    const appIdea = {
-        id: Math.random(),
-        name: "blog",
-        category: "full stack",
-        difficulty: "intermediate",
-        summary: "a blog web app",
-    };
 
-    // convert object to json
-    const dummyJSON = JSON.stringify(appIdea);
- 
-    // set the headers, response status and send the json data
-    res.setHeader("Content-Type", "application/json").status(200).send(dummyJSON);
-})
+app.get("/")
 
 // create server
 app.listen(5000, () => {
