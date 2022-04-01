6393bf10af361ddf97144a5f4631ccc8a101afe3
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-01 20:16:04 +0100

    Add documentation of the appController

--- backend/controller/appController.js
+++ backend/controller/appController.js
@@ -1,5 +1,11 @@
 // This is a demo controller
 
+/**
+ * 
+ * @param {Object} req This is the request object 
+ * @param {Object} res This is the response object
+ * @param {Function} next This function is used to call the next middleware
+ */
 module.exports.appController = (req, res, next) => {
-    
+
 }
