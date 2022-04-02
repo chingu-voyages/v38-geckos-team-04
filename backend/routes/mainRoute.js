0f4209a7995a016ccf80cad1f6e1c51cee7185d1
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-02 14:47:15 +0100

    Add comments for readability

--- backend/routes/mainRoute.js
+++ backend/routes/mainRoute.js
@@ -1,10 +1,15 @@
 //This is a demo route
 
+// import expressjs
 const express = require('express');
+
+// import appController
 const appController = require('../controller/appController');
 
+// initialize express Router
 const router = express.Router();
 
+// set up get route for visit to localhost:5000/
 router.get('/', appController);
 
 module.exports = router;
