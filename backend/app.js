abf3e612fdfd19617cd1007b385bb6b55a6c830d
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-03 21:17:47 +0100

    Setup, import and use environment variables

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 
 // import main route
@@ -5,6 +6,7 @@ const main = require('./routes/mainRoute');
 
 // expose the express api into the app const
 const app = express();
+const local = process.env.PORT;
 
 // parse application/json content-type
 app.use(express.json());
@@ -13,6 +15,6 @@ app.use(express.json());
 app.use("/", main);
 
 // create server
-app.listen(5000, () => {
-    console.log('Visit http://localhost:5000/ on a browser');
+app.listen(local, () => {
+    console.log(`Visit http://localhost:${local}/ on a browser`);
 });
