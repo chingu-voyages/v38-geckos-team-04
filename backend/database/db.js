ccf203350ff43d8b06fba339450755a48e2e63b4
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-03 08:15:08 +0100

    Add comments for readability

---
+++ backend/database/db.js
@@ -0,0 +1,7 @@
+/* 
+This is where the database connection lives
+The connection to mongoDB will be made here and then exported
+so that it can be used in other files where connection to the 
+database is required.
+
+*/
