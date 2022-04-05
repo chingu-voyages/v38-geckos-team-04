f3d1d545508a5fe1273e1365a5f5d5052f1c9c82
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-05 19:11:51 +0100

    Export database client

--- backend/database/db.js
+++ backend/database/db.js
@@ -12,7 +12,9 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.DB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
-  const collection = client.db("app").collection("projects");
-  // perform actions on the collection object
-  client.close();
+    const collection = client.db("app").collection("projects");
+    // perform actions on the collection object
+    client.close();
 });
+
+module.exports = client;
