8e0bfe5dc33bf42399a7e2efcbd816e2d668c00f
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-05 19:10:36 +0100

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -7,12 +7,12 @@ database is required.
 */
 
 require("dotenv").config();
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = process.env.DB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  const collection = client.db("app").collection("projects");
   // perform actions on the collection object
   client.close();
 });
