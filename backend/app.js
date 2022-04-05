ab544640593ceb7f305b325491b2d83e8f03be0f
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-05 20:32:11 +0100

    Create database connection

--- backend/app.js
+++ backend/app.js
@@ -18,12 +18,19 @@ app.use(express.json());
 // use the mainRoute
 app.use("/", main);
 
+const options = {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    // serverApi: ServerApiVersion.v1
+}
+
+
 // create server
 app.listen(port, () => {
     console.log(`Visit http://localhost:${port}/ on a browser`);
-    mongoose.connect(uri).then(result => {
-        console.log('Connected');
+    mongoose.connect(uri, options).then(result => {
+        console.log(result);
     }).catch(error => {
-        console.log('Not connected');
+        console.log(error);
     })
 });
