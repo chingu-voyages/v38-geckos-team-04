06e630db6e866e048145db3b4d97a8e79b465919
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-05 19:17:14 +0100

    Refactor mongo client connection

--- backend/database/db.js
+++ backend/database/db.js
@@ -11,10 +11,6 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const uri = process.env.DB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("app").collection("projects");
-    // perform actions on the collection object
-    client.close();
-});
+const db = client.db('app').collection('projects');
 
-module.exports = client;
+module.exports = db;
