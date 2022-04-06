e285f7779ba7dd2ac092668c65427e1cf4653ba5
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-06 16:00:14 +0100

    Delete header bg-video

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,12 +6,7 @@ import video from '../assets/coding-video.mp4';
 const Header = () => {
     return <Fragment>
         <header className='header'>
-            <div className="header__bg-video">
-                <video className="header__video" autoPlay muted loop>
-                    <source  src={video} type="video/mp4"/>
-                    Please, upgrade your browser to see this video
-                </video>
-            </div>
+            
         </header>
     </Fragment>
 }
