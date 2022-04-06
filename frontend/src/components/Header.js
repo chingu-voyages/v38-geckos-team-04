38145aa9d90810c852cc37ccfbb3a9500aa66bfa
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-06 16:00:53 +0100

    Remove redundant import statements

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,5 @@
 import { Fragment } from "react";
 import '../sass/components/header.scss';
-import '../sass/base/typography.scss';
-import video from '../assets/coding-video.mp4';
 
 const Header = () => {
     return <Fragment>
