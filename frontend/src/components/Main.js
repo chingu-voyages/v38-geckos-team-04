89de94d53ba250c17210a7c0ec079d4bd5553860
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-04-11 19:36:17 +0100

    Make changes according to the review

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,19 +1,19 @@
-import { Fragment, useState }from 'react'
+import { Fragment }from 'react'
 import SearchForm from './SearchForm';
 import ResultsList from './ResultsList';
 import '../sass/components/main.scss';
 
 function Main() {
-    const [radioDifficulty, setRadioDifficulty] = useState('beginner');
     
     
+
 
     return (
         <Fragment>
             <main className='main'>
             <h1 className='heading-primary'>abb - always be building</h1>
-                <SearchForm radioDifficulty={radioDifficulty} setRadioDifficulty={setRadioDifficulty}/>
-                <ResultsList radioDifficulty={radioDifficulty}/>
+                <SearchForm />
+                <ResultsList />
             </main>
         </Fragment>
     )
--- frontend/src/components/SearchForm.js
+++ frontend/src/components/SearchForm.js
@@ -1,15 +1,18 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import '../sass/components/searchform.scss';
 import '../sass/base/typography.scss';
 import icon from '../assets/search-icon.png';
 
-const SearchForm = ({radioDifficulty, setRadioDifficulty}) => (
+const SearchForm = () => {
+    const [radioDifficulty, setRadioDifficulty] = useState('beginner');
+
+    return (
     <Fragment>
          <div className="search-form">
             <div className="text-input-container">
-                <label for="name">Name</label>
+                <label htmlFor="name">Name</label>
                 <input type="text" id="name" name="name" placeholder="Search for an app idea or simply choose the difficulty level below..."/> 
-                <img classNameName="search-icon" src={icon} alt="search"/>
+                <img className="search-icon" src={icon} alt="search"/>
             </div>
 
             <div className="radio-bar-container">
@@ -22,8 +25,8 @@ const SearchForm = ({radioDifficulty, setRadioDifficulty}) => (
                         checked={radioDifficulty === 'beginner'} 
                         onClick={() => setRadioDifficulty('beginner')}
                     />
-                    <label for="beginner">
-                        <h2 classNameName="heading-secondary heading-radio">Beginner</h2>
+                    <label htmlFor="beginner">
+                        <h2 className="heading-secondary heading-radio">Beginner</h2>
                         <p>Developers in the early stages of their learning journey. Those who are typically focused on creating user-facing applications.</p>
                     </label>
                 </div>
@@ -36,7 +39,7 @@ const SearchForm = ({radioDifficulty, setRadioDifficulty}) => (
                         checked={radioDifficulty === 'intermediate'} 
                         onClick={() => setRadioDifficulty('intermediate')}
                     />
-                    <label for="intermediate">
+                    <label htmlFor="intermediate">
                     <h2 className="heading-secondary heading-radio">Intermediate</h2>
                         <p>Developers at an intermediate stage of learning and experience. They are comfortable in UI/UX, using development tools, and building apps that use API services.</p>
                     </label>
@@ -50,7 +53,7 @@ const SearchForm = ({radioDifficulty, setRadioDifficulty}) => (
                         checked={radioDifficulty === 'advanced'} 
                         onClick={() => setRadioDifficulty('advanced')}
                     />
-                    <label for="advanced">
+                    <label htmlFor="advanced">
                     <h2 className="heading-secondary heading-radio">Advanced</h2>
                         <p>Developers who have all of the above, and are learning more advanced techniques like implementing backend applications and database services.</p>
                     </label>
@@ -58,6 +61,6 @@ const SearchForm = ({radioDifficulty, setRadioDifficulty}) => (
             </div>      
          </div>
     </Fragment>
-)
+)}
 
 export default SearchForm;
