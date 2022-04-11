3d13ccf69ad7e77a5a0974709ed9fd155db0ce2f
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-04-11 11:35:11 +0100

    Change appList from Object to Array of Objects

--- frontend/src/components/ResultsList.js
+++ frontend/src/components/ResultsList.js
@@ -3,58 +3,56 @@ import '../sass/components/resultslist.scss';
 import '../sass/base/typography.scss';
 import Result from './Result';
 
-const appIdeas= {
-        app1: {
+const appIdeas= [
+        {
             name: "Bin2Dec",
             description: "Binary-to-Decimal number converter",
             link: "https://github.com/florinpop17/app-ideas/blob/master/Projects/1-Beginner/Bin2Dec-App.md",
             difficulty: "beginner",
         },
-        app2: {
+        {
             name: "Border Radius Previewer",
             description: "Preview how CSS3 border-radius values affect an element",
             link: "https://github.com/florinpop17/app-ideas/blob/master/Projects/1-Beginner/Border-Radius-Previewer.md",
             difficulty: "beginner",
         },
-        app3: {
+        {
             name: "Bit Masks",
             description: "Using Bit Masks for Conditions",
             link: "https://github.com/florinpop17/app-ideas/blob/master/Projects/2-Intermediate/Bit-Masks-App.md",
             difficulty: "intermediate",
         },
-        app4: {
+        {
             name: "Card Memory Game",
             description: "Memorize and match hidden images",
             link: "https://github.com/florinpop17/app-ideas/blob/master/Projects/2-Intermediate/Card-Memory-Game.md",
             difficulty: "intermediate",
         },
-        app5: {
+        {
             name: "Battleship Game Engine",
             description: "Create a callable engine to play the Battleship game",
             link: "https://github.com/florinpop17/app-ideas/blob/master/Projects/3-Advanced/Battleship-Game-Engine.md",
             difficulty: "advanced",
         },
-        app6: {
+        {
             name: "Shuffle Deck",
             description: "Evaluate different algorithms for shuffling a card deck",
             link: "https://github.com/florinpop17/app-ideas/blob/master/Projects/3-Advanced/Shuffle-Deck-App.md",
             difficulty: "advanced",
         },
-}
+    ]
 
 const ResultsList = ({difficulty}) => (
     <Fragment>
         <div className="results-container">
-            {Object.keys(appIdeas).map(function (app) { 
+            {appIdeas.map(app => 
 
-                if (appIdeas[app].difficulty === difficulty)
-                    return (  
                         <Result 
-                            name={appIdeas[app].name} 
-                            description={appIdeas[app].description} 
-                            link={appIdeas[app].link} 
-                        />)
-            }
+                            name={app.name} 
+                            description={app.description} 
+                            link={app.link} 
+                        />
+            
             )}   
         </div>
     </Fragment>
