3fcc27c645639065f3d3b784d6637da4b9c76535
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-14 20:37:59 +0100

    Update the navbar item classname

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,8 +7,8 @@ function Navbar() {
         <nav className='navbar'>
             <div className='navbar__logo'>develapp</div>
             <ul className='navbar__nav'>
-                <li className='nav-item'><Link to="/">Home</Link></li>
-                <li className='nav-item'><Link to="/about">About</Link></li>
+                <li className='nav__item'><Link to="/">Home</Link></li>
+                <li className='nav__item'><Link to="/about">About</Link></li>
                 {/* <li className='nav-item'><Link to="/learn">Learn</Link></li> */}
             </ul>
         </nav>    
