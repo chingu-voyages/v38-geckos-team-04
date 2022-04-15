07d4c6b8e119353cedc5d9e87e82db564a7dd594
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-15 11:24:51 +0100

    Update the about component

--- frontend/src/components/pages/About.js
+++ frontend/src/components/pages/About.js
@@ -1,10 +1,9 @@
 import { Fragment } from 'react';
-// How do I link to the scss file
 import '../../sass/components/about.scss';
 
 function About() {
     return <Fragment>
-        <div className='main' id='about'> 
+        <div className='about'> 
             One difficulty in learning how to code is figuring out which project to build to improve your skills. Most tutorials simply build a to-do app. Develapp takes you beyond to-do list apps and helps you find projects you can build and be proud to include in your portfolio. Develapp is made for you so that you can always be building. This project was built by a remote team during the chingu voyage 38. 
         </div>
     </Fragment>
