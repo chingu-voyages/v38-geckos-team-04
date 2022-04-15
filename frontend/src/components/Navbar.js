7ff03e364c7adef62da8deb654afcb0b489be19c
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-15 10:34:06 +0100

    Add navbar nav link class

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,8 +7,8 @@ function Navbar() {
         <nav className='navbar'>
             <div className='navbar__logo'>develapp</div>
             <ul className='navbar__nav'>
-                <li className='navbar__nav--item'><Link to="/">Home</Link></li>
-                <li className='navbar__nav--item'><Link to="/about">About</Link></li>
+                <li className='navbar__nav--item'><Link to="/" className='navbar__nav--link'>Home</Link></li>
+                <li className='navbar__nav--item'><Link to="/about" className='navbar__nav--link'>About</Link></li>
             </ul>
         </nav>    
     </Fragment>
