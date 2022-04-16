57a45ea954be9a27eb97118903d7cb4f2e1ff08f
Lauren Gainsbrook <lauren.gainsbrook1@gmail.com>
2022-04-16 22:01:20 +0200

    add DetailsView file

---
+++ frontend/src/components/DetailsView.js
@@ -0,0 +1,12 @@
+import { Fragment }from 'react'
+import '../sass/components/details-view.scss';
+
+const DetailsView = () => {
+    return <Fragment>
+        <div className='details-view'>
+        </div>
+        
+    </Fragment>
+}
+
+export default DetailsView;
