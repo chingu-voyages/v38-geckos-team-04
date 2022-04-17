a83fb6e8a16e9eed8e0e5188325f41d7e5dc56d1
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-04-17 21:07:17 +0100

    Remove mocked data

--- frontend/src/components/SearchForm.js
+++ frontend/src/components/SearchForm.js
@@ -3,53 +3,6 @@ import '../sass/components/searchform.scss';
 import '../sass/base/typography.scss';
 import icon from '../assets/search-icon.png';
 
-// Mocked data to work on before the fetch function is created. To be removed.
-//
-// const appIdeas= [
-//     {
-//         id: 1,
-//         name: "Bin2Dec",
-//         description: "Binary-to-Decimal number converter",
-//         link: "https://github.com/florinpop17/app-ideas/blob/master/Projects/1-Beginner/Bin2Dec-App.md",
-//         difficulty: "beginner",
-//     },
-//     {
-//         id: 2,
-//         name: "Border Radius Previewer",
-//         description: "Preview how CSS3 border-radius values affect an element",
-//         link: "https://github.com/florinpop17/app-ideas/blob/master/Projects/1-Beginner/Border-Radius-Previewer.md",
-//         difficulty: "beginner",
-//     },
-//     {
-//         id: 3,
-//         name: "Bit Masks",
-//         description: "Using Bit Masks for Conditions",
-//         link: "https://github.com/florinpop17/app-ideas/blob/master/Projects/2-Intermediate/Bit-Masks-App.md",
-//         difficulty: "intermediate",
-//     },
-//     {
-//         id: 4,
-//         name: "Card Memory Game",
-//         description: "Memorize and match hidden images",
-//         link: "https://github.com/florinpop17/app-ideas/blob/master/Projects/2-Intermediate/Card-Memory-Game.md",
-//         difficulty: "intermediate",
-//     },
-//     {
-//         id: 5,
-//         name: "Battleship Game Engine",
-//         description: "Create a callable engine to play the Battleship game",
-//         link: "https://github.com/florinpop17/app-ideas/blob/master/Projects/3-Advanced/Battleship-Game-Engine.md",
-//         difficulty: "advanced",
-//     },
-//     {
-//         id: 6,
-//         name: "Shuffle Deck",
-//         description: "Evaluate different algorithms for shuffling a card deck",
-//         link: "https://github.com/florinpop17/app-ideas/blob/master/Projects/3-Advanced/Shuffle-Deck-App.md",
-//         difficulty: "advanced",
-//     },
-// ]
-
 const SearchForm = ({setResultsDataHandler}) => {
     const [radioDifficulty, setRadioDifficulty] = useState('beginner');
 
