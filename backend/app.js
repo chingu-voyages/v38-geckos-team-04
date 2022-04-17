24ffe448eb9500e93ebd128c7765fdd25a95bf51
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-17 21:28:51 +0100

    Import path module

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const { ServerApiVersion } = require('mongodb');
 const mongoose = require('mongoose');
+const path = require('path');
 
 // import main route
 const API = require('./routes/api');
