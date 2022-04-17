db4a463797df3187f055ffebd2dcd7f2f89fdfe0
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-04-17 19:13:25 +0100

    Refactor: Changed the way of setting setHeader/header/send

--- backend/controller/apiControl.js
+++ backend/controller/apiControl.js
@@ -31,22 +31,22 @@ const apiControl = (req, res) => {
         console.log('Payload: ',name);
 
         // check res
-        console.log('Res: ', res);
+        // console.log('Res: ', res);
 
         // query the database to find data matching the request
         Project.find({tier: name}).exec((err, result) => {
                 // add better error handling
 
                 //check if the data is fetched correctly
-                console.log(result);
+                // console.log(result);
 
                 if (err) return;
 
 
                 // set headers, status and send json data
-                res.setHeader("Content-Type", "application/json")
-                // .status(200)
-                .send(result);
+                res.setHeader("Content-Type", "application/json");
+                res.status(200);
+                res.send(result);
         });
 }
 
